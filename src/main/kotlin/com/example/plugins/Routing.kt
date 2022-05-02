1fa3740c7352e0b264b5124fe4350322ee6e83ed
wkddnjsgh <luca4759@gmail.com>
2022-05-02 15:01:59 +0900

    Add CustomerRouting

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,13 +1,11 @@
 package com.example.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
+import com.example.routes.customerRouting
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-
     routing {
+        customerRouting()
     }
 }
