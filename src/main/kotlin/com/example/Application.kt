706025f62e08473b580e945bf8bf2281e5230fd8
wkddnjsgh <luca4759@gmail.com>
2022-05-02 15:04:10 +0900

    Add static import

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -2,9 +2,10 @@ package com.example
 
 import io.ktor.server.application.*
 import com.example.plugins.*
+import io.ktor.server.netty.*
 
 fun main(args: Array<String>): Unit =
-    io.ktor.server.netty.EngineMain.main(args)
+    EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
