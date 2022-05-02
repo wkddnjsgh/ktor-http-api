d69530ab83e24be3e0890aa244f850450a8ac6bb
wkddnjsgh <luca4759@gmail.com>
2022-05-02 17:14:39 +0900

    Add OrderRouteTest

--- src/main/kotlin/com/example/plugins/Serialization.kt
+++ src/main/kotlin/com/example/plugins/Serialization.kt
@@ -1,12 +1,8 @@
 package com.example.plugins
 
 import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
-import io.ktor.server.routing.*
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
